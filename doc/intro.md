6dcaab3a722e5cba27ba2e8d89fc06ad502ec1c1
Brandon Bloom <brandon@brandonbloom.name>
2012-11-22 01:04:22 -0800

    lein init backtick

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to backtick
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/backtick/core_test.clj
@@ -0,0 +1,7 @@
+(ns backtick.core-test
+  (:use clojure.test
+        backtick.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
