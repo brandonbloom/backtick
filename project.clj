df10ac60db6c841b2021b7f16a8d2b358cfeebe5
Brandon Bloom <brandon@brandonbloom.name>
2012-11-22 01:34:21 -0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject backtick "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Provides the syntax-quote reader macro as a normal macro"
+  :url "https://github.com/brandonbloom/backtick"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
