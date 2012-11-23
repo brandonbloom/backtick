4201400fadf7abc9a725fe1d4cb4865406b0c421
Brandon Bloom <brandon@brandonbloom.name>
2012-11-23 03:38:26 -0800

    Test defquote

--- test/backtick_test.clj
+++ test/backtick_test.clj
@@ -1,9 +1,16 @@
 (ns backtick-test
   (:use clojure.test)
-  (:require [backtick :refer (template)]))
+  (:require [backtick :refer (template defquote)]))
 
 (deftest template-test
   (testing "Primitives, collections, unquote, and splice; symbols qualified"
     (let [n 5 v [:a :b]]
       (is (=           `(5 nil true a/b ~n [p/q ~@v r/s] {:x #{"s"}})
               (template (5 nil true a/b ~n [p/q ~@v r/s] {:x #{"s"}})))))))
+
+(defquote wacky-quote (fn [sym] (symbol (str sym "!"))))
+
+(deftest defquote-test
+  (testing "Customer resolver"
+    (is (= '(foo! :a [5 bar!])
+           (wacky-quote (foo :a [5 bar]))))))
