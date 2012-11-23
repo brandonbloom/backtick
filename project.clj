1229c96e25991538b92180953ea57b8257c083a2
Brandon Bloom <brandon@brandonbloom.name>
2012-11-23 13:50:21 -0800

    Increment version to 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject backtick "0.1.0"
+(defproject backtick "0.2.0-SNAPSHOT"
   :description "Provides the syntax-quote reader macro as a normal macro"
   :url "https://github.com/brandonbloom/backtick"
   :license {:name "Eclipse Public License"
