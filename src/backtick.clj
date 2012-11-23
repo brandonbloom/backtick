db49800a42c221085a6a516d4158942938692a8c
Brandon Bloom <brandon@brandonbloom.name>
2012-11-23 03:35:49 -0800

    Correctly working pluggable resolver via defquote

--- src/backtick.clj
+++ src/backtick.clj
@@ -1,7 +1,7 @@
 (ns backtick
   (:refer-clojure :exclude [eval resolve]))
 
-(defn resolve [sym] sym)
+(def ^:dynamic *resolve*)
 
 (defn- unquote? [form]
   (and (seq? form) (= (first form) 'clojure.core/unquote)))
@@ -11,15 +11,16 @@
 
 (defn syntax-quote-fn [form]
   (cond
-    (symbol? form) `'~(resolve form)
+    (symbol? form) `'~(*resolve* form)
     (unquote? form) (second form)
     (unquote-splicing? form) (throw (Exception. "splice not in list"))
     (coll? form)
       (let [xs (if (map? form) (apply concat form) form)
-            cat `(concat ~@(for [x (partition-by unquote-splicing? xs)]
-                             (if (unquote-splicing? (first x))
-                               (second (first x))
-                               `[~@(map syntax-quote-fn x)])))]
+            parts (for [x (partition-by unquote-splicing? xs)]
+                    (if (unquote-splicing? (first x))
+                      (second (first x))
+                      (mapv syntax-quote-fn x)))
+            cat (doall `(concat ~@parts))]
         (cond
           (vector? form) `(vec ~cat)
           (map? form) `(apply hash-map ~cat)
@@ -28,5 +29,10 @@
           :else (throw (Exception. "Unknown collection type"))))
     :else `'~form))
 
-(defmacro syntax-quote [form]
-  (syntax-quote-fn form))
+(defmacro defquote [name resolver]
+  `(let [resolver# ~resolver]
+     (defmacro ~name [form#]
+       (binding [*resolve* resolver#]
+         (syntax-quote-fn form#)))))
+
+(defquote template identity)
--- test/backtick_test.clj
+++ test/backtick_test.clj
@@ -1,9 +1,9 @@
 (ns backtick-test
   (:use clojure.test)
-  (:require [backtick :refer (syntax-quote)]))
+  (:require [backtick :refer (template)]))
 
-(deftest syntax-quote-test
+(deftest template-test
   (testing "Primitives, collections, unquote, and splice; symbols qualified"
     (let [n 5 v [:a :b]]
-      (is (=               `(5 nil true a/b ~n [p/q ~@v r/s] {:x #{"s"}})
-              (syntax-quote (5 nil true a/b ~n [p/q ~@v r/s] {:x #{"s"}})))))))
+      (is (=           `(5 nil true a/b ~n [p/q ~@v r/s] {:x #{"s"}})
+              (template (5 nil true a/b ~n [p/q ~@v r/s] {:x #{"s"}})))))))
