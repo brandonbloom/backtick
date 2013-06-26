4672faf9219a4a714afa27fda25da205d29dc60a
Brandon Bloom <brandon@brandonbloom.name>
2013-06-26 13:35:03 -0400

    Upgrade to Clojure 1.5.1

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "https://github.com/brandonbloom/backtick"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
