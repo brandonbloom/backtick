92022e6ff3d64e2b7bbacf89c57c1ad3f6373eaf
Noah Bogart <noah.bogart@hey.com>
2023-08-15 22:40:33 -0400

    Fix #5, changes in ns-resolve

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "https://github.com/brandonbloom/backtick"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
+             :1.11 {:dependencies [[org.clojure/clojure "1.11.1"]]}}
+  :aliases {"test-all" ["with-profile" "dev:dev,1.7:dev,1.8:dev,1.9:dev,1.10:dev,1.11" "test"]})
